d6592ccf714eeb59f1aa6e72373f03271b6e4f2b
Ariel Rey <ariel.rey@mercadolibre.com>
2019-08-24 12:01:11 -0300

    Adding test credentials

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const mercadopago = require('mercadopago');
 const app = express();
 
 // Set the mercadopago credentials
-mercadopago.configurations.setAccessToken('TEST-********');
+mercadopago.configurations.setAccessToken('TEST-169606388010973-082414-1c20929a9443e6f84e4f7a855affe0a8-464359136');
 
 // Attach the body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
