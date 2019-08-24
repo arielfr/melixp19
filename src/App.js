5cdad96b312c4fe40f3d28531fa1b159bf789915
Ariel Rey <ariel.rey@mercadolibre.com>
2019-08-24 12:06:05 -0300

    Changing default email

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
                 <input
                   id="email"
                   name="email"
-                  value="test_user_71425066@testuser.com"
+                  defaultValue="test_user_71425066@testuser.com"
                   type="email"
                   placeholder="your email"
                 />
